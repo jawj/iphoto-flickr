b27d0354cfd1b95b1d13428a87c513228877c8c7
George MacKerron <george@mackerron.co.uk>
2013-07-11 20:04:13 +0100

    Refactoring complete

--- flickrbackup.rb
+++ flickrbackup.rb
@@ -3,82 +3,70 @@
 
 %w{flickraw-cached tempfile fileutils yaml}.each { |lib| require lib }
 
+
+# own records setup
+
 dataDirName = File.expand_path "~/Library/Application Support/flickrbackup"
 FileUtils.mkpath dataDirName
 
-ID_SEP = ' -> '
-AS_SEP = "\u0000"
-
-class StoredStringHash
+class PersistedIDsHash
+  ID_SEP = ' -> '
   def initialize(fileName)
     @hash = {}
     FileUtils.touch fileName
     open(fileName).each_line do |line|
       k, v = line.chomp.split ID_SEP
-      @hash[k] = v
+      store k, v
     end
-    open(fileName, 'a') do |f|
-      @storage = f
+    open(fileName, 'a') do |file|
+      @file = file
       yield self
     end
   end
-  def add(k, v, fsync = true)
-    @hash[k] = v
-    dataRecord = "#{id1}#{ID_SEP}#{id2}"
-    @storage.puts dataRecord
-    fp.fsync if fsync
-    dataRecord
+  def add(k, v, doFsync = true)
+    store k, v
+    record = "#{k}#{ID_SEP}#{v}"
+    @file.puts record
+    fsync if doFsync
+    record
   end
   def get(k)
     @hash[k]
   end
+  def fsync
+    @file.fsync
+  end
+private
+  def store(k, v)
+    @hash[k] = v
+  end
 end
 
-class StoredAssociations < StoredStringHash
-  def associate(k, v)
-    @hash[[k, v].sort.join(':')] = 'Y'
-  end
+class PersistedIDsHashMany < PersistedIDsHash
   def associated?(k, v)
-    @hash[[k, v].sort.join(':')]
+    @hash[k] && @hash[k][v]
+  end
+private
+  def store(k, v)
+    (@hash[k] ||= {})[v] = true
   end
 end
 
-def loadIDsFileNamed(fileName, many2many = false)
-  FileUtils.touch fileName
-  idHash = {}
-  fileData = open(fileName).each_line do |line| 
-    id1, id2 = line.chomp.split ID_SEP
-    many2many ? (idHash[id1] ||= {})[id2] = true : idHash[id1] = id2
-  end
-  idHash
-end
 
-def appendToIDsFile(fp, id1, id2, fsync = true)
-  dataRecord = "#{id1}#{ID_SEP}#{id2}"
-  fp.puts dataRecord
-  fp.fsync if fsync
-  dataRecord
-end
+# AppleScript setup
+
+AS_SEP = "\u0000"
 
 def applescript(source, *args)
   IO.popen(['osascript', '-', *args], 'w') { |io| io.write(source) }
 end
 
-def loadASOutputFileNamed(fileName)
+def loadOutputFile(fileName)
   open(fileName, 'rb:utf-16be:utf-8').each_line(AS_SEP).map { |datum| datum.chomp AS_SEP }
 end
 
-def takeLongEnough
-  startTime = Time.now
-  returnValue = yield
-  timeTaken = Time.now - startTime
-  timeToSleep = 1.01 - timeTaken  # rate limit to just under 3600 reqs/hour
-  sleep timeToSleep if timeToSleep > 0
-  returnValue
-end
 
-
-# load or get Flickr credentials
+# Flickr API setup
 
 credentialsFileName = "#{dataDirName}/credentials.yaml"
 
@@ -118,16 +106,21 @@ else
   File.open(credentialsFileName, 'w') { |credentialsFile| YAML.dump(credentials, credentialsFile) }
 end
 
+def rateLimit
+  startTime = Time.now
+  returnValue = yield
+  timeTaken = Time.now - startTime
+  timeToSleep = 1.01 - timeTaken  # rate limit to just under 3600 reqs/hour
+  sleep timeToSleep if timeToSleep > 0
+  returnValue
+end
+
 
 # load own backup records
 
-uploadedPhotosFileName = "#{dataDirName}/uploaded-photo-ids-map.txt"
-createdAlbumsFileName  = "#{dataDirName}/created-album-ids-map.txt"
-photosInAlbumsFileName = "#{dataDirName}/photos-in-album-ids-map.txt"
-
-uploadedPhotosHash  = loadIDsFileNamed uploadedPhotosFileName
-createdAlbumsHash   = loadIDsFileNamed createdAlbumsFileName
-photosInAlbumsHash  = loadIDsFileNamed photosInAlbumsFileName, true
+PersistedIDsHash.new("#{dataDirName}/uploaded-photo-ids-map.txt") do |uploadedPhotos|
+PersistedIDsHash.new("#{dataDirName}/created-album-ids-map.txt") do |createdAlbums|
+PersistedIDsHashMany.new("#{dataDirName}/photos-in-album-ids-map.txt") do |photosInAlbums|
 
 
 # get all iPhoto IDs and paths, and filter out those already backed up
@@ -157,12 +150,12 @@ idsFile   = Tempfile.new 'ids'
 pathsFile = Tempfile.new 'paths'
 [idsFile, pathsFile].each { |f| f.close }
 applescript(photosAS, idsFile.path, pathsFile.path)
-allIDs   = loadASOutputFileNamed(idsFile).map { |id| id.to_f.to_i.to_s }
-allPaths = loadASOutputFileNamed(pathsFile)
+allIDs   = loadOutputFile(idsFile).map { |id| id.to_f.to_i.to_s }
+allPaths = loadOutputFile(pathsFile)
 [idsFile, pathsFile].each { |f| f.unlink }
 
 allPhotoData = allIDs.zip(allPaths)
-newPhotoData = allPhotoData.reject { |photoData| uploadedPhotosHash[photoData.first] }
+newPhotoData = allPhotoData.reject { |photoData| uploadedPhotos.get photoData.first }
 
 puts "\n#{allPhotoData.length} photos in iPhoto library"
 puts "#{newPhotoData.length} photos not yet uploaded to flickr\n"
@@ -209,7 +202,7 @@ end run
 albumsFile = Tempfile.new 'albums'
 albumsFile.close
 applescript(albumsAS, albumsFile.path)
-rawAlbumData = loadASOutputFileNamed(albumsFile)
+rawAlbumData = loadOutputFile(albumsFile)
 albumsFile.unlink
 
 albumEnum = rawAlbumData.each
@@ -228,85 +221,75 @@ end
 
 # upload new files
 
-open(uploadedPhotosFileName, 'a') do |uploadedPhotosFile|
-
-  newPhotoData.each_with_index do |photoData, i|
-    iPhotoID, photoPath = photoData
-    
-    begin
-      print "#{i + 1}. Uploading '#{photoPath}' ... "
-      flickrID = takeLongEnough { flickr.upload_photo photoPath }
-    
-    # keep trying in face of network errors: Timeout::Error, Errno::BROKEN_PIPE, SocketError, ...
-    rescue => err  
-      print "#{err.message}: retrying in 10s "; 10.times { sleep 1; print '.' }; puts
-      retry
-    end
-    puts appendToIDsFile(uploadedPhotosFile, iPhotoID, flickrID)
-    uploadedPhotosHash[iPhotoID] = flickrID  # in case this photo is in albums later
-
+newPhotoData.each_with_index do |photoData, i|
+  iPhotoID, photoPath = photoData
+  
+  begin
+    print "#{i + 1}. Uploading '#{photoPath}' ... "
+    flickrID = rateLimit { flickr.upload_photo photoPath }
+  
+  # keep trying in face of network errors: Timeout::Error, Errno::BROKEN_PIPE, SocketError, ...
+  rescue => err  
+    print "#{err.message}: retrying in 10s "; 10.times { sleep 1; print '.' }; puts
+    retry
   end
+
+  puts uploadedPhotos.add iPhotoID, flickrID
 end
 
 
 # update albums/photosets
 
-SET_NOT_FOUND         = 1
-PHOTO_NOT_FOUND       = 2
-PHOTO_ALREADY_IN_SET  = 3
+SET_NOT_FOUND   = 1
+PHOTO_NOT_FOUND = 2
 
 puts "\n#{albumData.length} standard albums in iPhoto\n"
 
-open(createdAlbumsFileName, 'a') do |createdAlbumsFile|
-open(photosInAlbumsFileName, 'a') do |photosInAlbumsFile|
+albumData.each do |albumID, album|
+  photosetID = createdAlbums.get albumID
 
-  albumData.each do |albumID, album|
-    photosetID = createdAlbumsHash[albumID]
+  if photosetID.nil?
+    print "Creating new photoset: '#{album[:name]}' ... "
+    somePhotoID = album[:photoIDs].first
+    someFlickrPhotoID = uploadedPhotos.get somePhotoID
 
-    if photosetID.nil?
-      print "Creating new photoset: '#{album[:name]}' ... "
-      somePhotoID = album[:photoIDs].first
-      someFlickrPhotoID = uploadedPhotosHash[somePhotoID]
+    begin
+      photosetID = rateLimit { flickr.photosets.create(title: album[:name], primary_photo_id: someFlickrPhotoID).id }     
+    rescue FlickRaw::FailedResponse => e
+      if e.code == PHOTO_NOT_FOUND  # photoset cannot be created if primary photo has been deleted from Flickr
+        print e.msg, ' ... '
+        photosetID = 'X'
+      else raise e
+      end
+    end
+
+    puts createdAlbums.add albumID, photosetID
+    photosInAlbums.add somePhotoID, albumID
+  end
+
+  # add any new photos
+  album[:photoIDs].each do |iPhotoID|
+
+    unless photosInAlbums.associated? iPhotoID, albumID
+      flickrPhotoID = uploadedPhotos.get iPhotoID
+      print "Adding photo #{iPhotoID} -> #{flickrPhotoID} to photoset #{albumID} -> #{photosetID} ... "
+      errorHappened = false
 
       begin
-        photosetID = takeLongEnough { flickr.photosets.create(title: album[:name], primary_photo_id: someFlickrPhotoID).id }     
+        rateLimit { flickr.photosets.addPhoto(photoset_id: photosetID, photo_id: flickrPhotoID) }
       rescue FlickRaw::FailedResponse => e
-        if e.code == PHOTO_NOT_FOUND  # photoset cannot be created if primary photo has been deleted from Flickr
-          print e.msg, ' ... '
-          photosetID = 'X'
+        if [SET_NOT_FOUND, PHOTO_NOT_FOUND].include? e.code  
+          puts e.msg
+          errorHappened = true
         else raise e
         end
       end
-# ADD RECORDS OF PRIMARY PHOTO BEING ADDED -- & REMOVE PHOTO_ALREADY_IN_SET BELOW?
-      puts appendToIDsFile(createdAlbumsFile, albumID, photosetID)
+
+      puts "done" unless errorHappened
+      photosInAlbums.add iPhotoID, albumID
     end
-
-    # add any new photos
-    album[:photoIDs].each do |iPhotoID|
-      albumsForPhoto = photosInAlbumsHash[iPhotoID]
-      photoIsInAlbum = albumsForPhoto && albumsForPhoto[albumID]
-
-      unless photoIsInAlbum
-        flickrPhotoID = uploadedPhotosHash[iPhotoID]
-        print "Adding photo #{iPhotoID} -> #{flickrPhotoID} to photoset #{albumID} -> #{photosetID} ... "
-        errorHappened = false
-
-        begin
-          takeLongEnough { flickr.photosets.addPhoto(photoset_id: photosetID, photo_id: flickrPhotoID) }
-        rescue FlickRaw::FailedResponse => e
-          if [SET_NOT_FOUND, PHOTO_NOT_FOUND, PHOTO_ALREADY_IN_SET].include? e.code  
-            puts e.msg
-            errorHappened = true
-          else raise e
-          end
-        end
-
-        puts "done" unless errorHappened
-        appendToIDsFile(photosInAlbumsFile, iPhotoID, albumID)
-      end
-    end
-
   end
 
 end
-end
+
+end; end; end  # own records blocks
